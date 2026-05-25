bee90f90d22b85e46826e442adc4d20aefea4fe7
Kushang Acharya <kushangacharya8830@gmail.com>
2026-05-26 00:35:21 +0530

    optimize whatsapp client memory and pin webVersionCache

--- src/services/whatsappService.js
+++ src/services/whatsappService.js
@@ -50,7 +50,7 @@ async function initializeWhatsAppClient() {
             }),
             webVersionCache: {
                 type: 'remote',
-                remotePath: 'https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/{version}.html',
+                remotePath: 'https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html',
             },
             userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36',
             puppeteer: {
@@ -62,6 +62,7 @@ async function initializeWhatsAppClient() {
                     '--disable-accelerated-2d-canvas',
                     '--no-first-run',
                     '--no-zygote',
+                    '--single-process',
                     '--disable-gpu',
                     '--disable-software-rasterizer'
                 ],
