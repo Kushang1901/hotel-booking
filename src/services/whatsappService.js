f3cbf3305b427befb417ec393bb502308f5a7658
Kushang Acharya <kushangacharya8830@gmail.com>
2026-05-25 23:43:57 +0530

    check by cache

--- src/services/whatsappService.js
+++ src/services/whatsappService.js
@@ -48,6 +48,11 @@ async function initializeWhatsAppClient() {
             authStrategy: new LocalAuth({
                 clientId: sessionModel.CLIENT_ID
             }),
+            webVersionCache: {
+                type: 'remote',
+                remotePath: 'https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/{version}.html',
+            },
+            userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36',
             puppeteer: {
                 headless: true,
                 args: [
